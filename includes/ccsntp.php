2a202648e3bf10b991a3060e8a6b93bba9f97979
MILAN88888 <chaudharymilan996@gmail.com>
2024-07-22 22:04:45 +0545

    Add - Front setup file

---
+++ includes/ccsntp.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Final class of custom code snippet plugin.
+ *
+ * @since 1.0.0
+ * @package CCSNTP\
+ */
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    publicPath: "/build/"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
+  ]
+};
