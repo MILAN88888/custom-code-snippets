94915cc2d555c77db4a93cb4b59b1f1686cc8854
MILAN88888 <chaudharymilan996@gmail.com>
2024-08-08 08:32:58 +0545

    Fix - Ref code editor issue

--- src/screen/addnew/components/CodeMirrorEditor.tsx
+++ src/screen/addnew/components/CodeMirrorEditor.tsx
@@ -1,10 +1,35 @@
-import React from "react";
-import Editor, { EditorProps } from "@monaco-editor/react";
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import Editor, { EditorProps } from '@monaco-editor/react';
+import { editor as MonacoEditor } from 'monaco-editor';
 
-interface CodeMirrorEditorProps extends EditorProps {}
+interface CodeMirrorEditorProps extends EditorProps {
+  onMount?: (editor: MonacoEditor.IStandaloneCodeEditor) => void;
+}
 
-const CodeMirrorEditor: React.FC<CodeMirrorEditorProps> = (props) => {
-  return <Editor {...props} />;
-};
+export interface CodeMirrorEditorHandle {
+  editor: MonacoEditor.IStandaloneCodeEditor | null;
+}
+
+const CodeMirrorEditor = forwardRef<CodeMirrorEditorHandle, CodeMirrorEditorProps>((props, ref) => {
+  const editorRef = useRef<MonacoEditor.IStandaloneCodeEditor | null>(null);
+
+  useImperativeHandle(ref, () => ({
+    editor: editorRef.current
+  }));
+
+  return (
+    <Editor
+      {...props}
+      onMount={(editor) => {
+        editorRef.current = editor;
+        if (props.onMount) {
+          props.onMount(editor);
+        }
+      }}
+    />
+  );
+});
+
+CodeMirrorEditor.displayName = 'CodeMirrorEditor';
 
 export default CodeMirrorEditor;
