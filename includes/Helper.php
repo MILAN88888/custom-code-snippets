9da70eb0ce0fdc2e3e1e2bf28603c27d821d40e2
milan88888 <chaudharymilan996@gmail.com>
2024-08-29 22:04:09 +0545

    Add - Function to execute snippet for testing

--- includes/Helper.php
+++ includes/Helper.php
@@ -25,4 +25,31 @@ class Helper {
 	public static function plugin_url( $path = '/' ) {
 		return untrailingslashit( plugins_url( $path, CCSNPT_PLUGIN_FILE ) );
 	}
+	/**
+	 * Execute the code snippets.
+	 *
+	 * @param [string]  $code The code.
+	 * @param [integer] $id The snippet id.
+	 * @return string|boolean
+	 */
+	public static function execute_snippet( $code, $id = 0 ) {
+		if ( empty( $code ) ) {
+			return false;
+		}
+
+		ob_start();
+
+		try {
+			$result = @eval( $code );
+
+		} catch ( Throwable $e ) {
+			$result = $e->getMessage();
+
+			ob_end_clean();
+
+		}
+		do_action( 'ccsnpt_after_execute_snippet', $code, $id, $result );
+
+		return $result;
+	}
 }
