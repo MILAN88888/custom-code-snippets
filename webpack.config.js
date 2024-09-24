c350fc650dbbb00374b9b3599e769cda12aaf2c8
milan88888 <chaudharymilan996@gmail.com>
2024-09-24 22:04:01 +0545

    Add - Update web packages

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,11 @@ module.exports = (env, argv) => {
           use: {
             loader: "babel-loader",
             options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript"
+              ],
               plugins: isDevelopment
                 ? [require.resolve("react-refresh/babel")]
                 : []
